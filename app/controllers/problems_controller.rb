1a1cec9c5fe14a55bb265517a6505b0b0c6e7895
yokki <ohayokki@yahoo.co.jp>
2019-08-24 22:41:01 +0900

    correct_user

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,5 +1,6 @@
 class ProblemsController < ApplicationController
-  #before_action correct_user, only: [:create,update,destroy]
+  before_action :correct_user, only: [:create, :update, :destroy]
+
   def index
     @problem = Problem.new
     @problems = Problem.all.order(created_at: :desc)
