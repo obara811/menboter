2a7981a96cc13d83d38d546b7fecae896532c6c0
ohayokki <oahyokki@gmail.com>
2021-03-07 17:30:22 +0900

    apikey sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-  :user_name => "apikey",
-  :password => ENV['SENDGRID_PASSWORD'],
+  # :user_name => "ENV['SENDGRID_USERNAME']",
+  # :password => ENV['SENDGRID_PASSWORD'],
+  :user_name      => 'apikey',
+  :password       => ENV['SENDGRID_APIKEY'],
   :domain => 'herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
