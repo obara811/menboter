a4c525fab49cab44084bfc40565734f640f55090
ohayokki <oahyokki@gmail.com>
2021-03-07 17:25:11 +0900

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
+  :user_name => "apikey",
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'herokuapp.com',
   :address => 'smtp.sendgrid.net',
