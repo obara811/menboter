04fbeec9eab5ce8ab16a4cff48c775c75ac6dfbc
mentor-obara <ohayokki@gmail.com>
2022-12-12 17:29:39 +0900

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,15 +69,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-  # :user_name => "ENV['SENDGRID_USERNAME']",
-  # :password => ENV['SENDGRID_PASSWORD'],
-  :user_name      => 'apikey',
-  :password       => ENV['SENDGRID_API_KEY'],
-  :domain => 'herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV['USER_EMAIL'],
+    password:             ENV['USER_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto:  true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -106,6 +104,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://menboter.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'http://160.251.80.99/' }
 
 end
